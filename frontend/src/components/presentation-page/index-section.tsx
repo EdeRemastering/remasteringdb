f686a9f2e9487817656707486256bb9e2a935a62
Eder Mestra <146907295+EdeRemastering@users.noreply.github.com>
2025-07-22 11:49:21 -0500

    style: apply conventional gradient
    Use the conventional gradient from blue 500 to violet 600

--- frontend/src/components/presentation-page/index-section.tsx
+++ frontend/src/components/presentation-page/index-section.tsx
@@ -8,11 +8,11 @@ export const IndexSection = () => {
         <Badge className='bg-blue-50 border border-blue-500 font-bold text-blue-500 px-4 py-2 rounded-full'><User2 /> Used by 1000+ developers</Badge>
         <div className="flex flex-col gap-10 w-[80vw]">
         <h1 className='text-center text-7xl font-bold'>Manage your databases visually with ease using 
-            <span className='bg-gradient-to-r from-blue-500 to-blue-700 text-transparent bg-clip-text'> Remastering</span><span>DB</span></h1>
+            <span className='bg-gradient-to-r from-blue-500 to-violet-600 text-transparent bg-clip-text'> Remastering</span><span>DB</span></h1>
         <p className='text-center text-2xl'>A modern and intuitive tool for visualizing, managing, and developing your databases without writing code. Perfect for developers and analysts.</p>
         </div>
         <div className='flex items-center gap-4'>
-            <button className='bg-gradient-to-r from-blue-500 to-blue-700  font-bold text-white px-4 py-2 rounded-md'>Get Started</button>
+            <button className='bg-gradient-to-r from-blue-500 to-violet-600  font-bold text-white px-4 py-2 rounded-md'>Get Started</button>
             <button className='font-bold px-4 py-2 rounded-md border border-gray-400'>Demo</button>
         </div>
     </div>
--- frontend/src/components/presentation-page/presentation-navbar.tsx
+++ frontend/src/components/presentation-page/presentation-navbar.tsx
@@ -6,7 +6,7 @@ export const PresentationNavbar = () => {
     <div className='flex justify-around items-center bg-white w-full h-[60px] fixed top-0 z-50 shadow-md'>
         <div className="icon">
             <p className='font-bold'>
-                <span className='bg-gradient-to-r from-blue-500 to-blue-700 text-transparent bg-clip-text'>Remastering</span> 
+                <span className='bg-gradient-to-r from-blue-500 to-violet-600 text-transparent bg-clip-text'>Remastering</span> 
             <span className='text-black'>DB</span></p>
         </div>
         <div className="flex items-center text-[15px] gap-4">
@@ -30,7 +30,7 @@ export const PresentationNavbar = () => {
         </div>
         <div className="buttons flex items-center gap-4">
             <button className='font-bold'>Login</button>
-            <button className='font-bold bg-gradient-to-r from-blue-400 to-blue-800  text-white px-4 py-2 rounded-md'>Start for free</button>
+            <button className='font-bold bg-gradient-to-r from-blue-500 to-violet-600  text-white px-4 py-2 rounded-md'>Start for free</button>
         </div>
     </div>
   )
